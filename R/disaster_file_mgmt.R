3f3694cd32a8771b6b87ec4e014a08fed7ead6ad
vnssyl <vanessa.y.liao@gmail.com>
2024-09-23 10:34:16 -0400

    complete disaster file

---
+++ R/disaster_file_mgmt.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(here)
+raw_data <- read.csv(here('original','disaster.csv'),header=TRUE)
+
+clean_data <- filter(raw_data, Year >= 2000 & Year <= 2019,
+                     Disaster.Type== "Earthquake" | Disaster.Type == "Drought")
+
+clean_data <- clean_data %>% 
+  select("Year","ISO","Disaster.Type")
+
+clean_data$earthquake <- ifelse(clean_data$Disaster.Type=="Earthquake",1,0)
+clean_data$drought <- ifelse(clean_data$Disaster.Type == "Drought",1,0)
