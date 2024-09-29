48b90e1a9f75a11d1faa4f91c0f58b93205f0d01
vnssyl <vanessa.y.liao@gmail.com>
2024-09-29 14:20:52 -0400

    fixed disaster file

--- R/disaster_file_mgmt.R
+++ R/disaster_file_mgmt.R
@@ -4,17 +4,13 @@ library(here)
 # Load dataset 
 disaster_raw <- read.csv(here('original','disaster.csv'),header=TRUE)
 
-# Filter by year and disaster type
-disaster_clean <- filter(disaster_raw, Year >= 2000 & Year <= 2019,
-                     Disaster.Type== "Earthquake" | Disaster.Type == "Drought")
+disaster_clean <- filter(disaster_raw, Year %in% 2000:2019, Disaster.Type %in% c('Earthquake', 'Drought'))
+disaster_clean <- dplyr::select(disaster_clean, c('Year', 'ISO', 'Disaster.Type')) %>%
+  mutate(drought = if_else(Disaster.Type == 'Drought', 1, 0)) %>%
+  mutate(earthquake = if_else(Disaster.Type == 'Earthquake', 1, 0))
 
-# Select variables
-disaster_clean <- disaster_clean %>% 
-  select("Year","ISO","Disaster.Type")
-
-# Create dummy var for earthquake and drought
-disaster_clean$earthquake <- ifelse(disaster_clean$Disaster.Type=="Earthquake",1,0)
-disaster_clean$drought <- ifelse(disaster_clean$Disaster.Type == "Drought",1,0)
+disaster_clean <- disaster_clean %>% group_by(Year, ISO) %>%
+  summarise(drought = max(drought), earthquake = max(earthquake))
 
 # Write CSV file
 write.csv(disaster_clean,here('data','disaster_clean.csv'))
