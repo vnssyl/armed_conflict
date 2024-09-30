f3085a9e659ac06c815357bfff0d13ac8fc60db4
vnssyl <vanessa.y.liao@gmail.com>
2024-09-30 10:32:00 -0400

    removed conflict_id from dataset

--- R/conflict_data_mgmt.R
+++ R/conflict_data_mgmt.R
@@ -1,10 +1,14 @@
 library(tidyverse)
 library(here)
 
+# load dataset
 conflict_raw <- read.csv(here('original','conflictdata.csv'),header=TRUE)
 
-conflict_raw$conflict_bin <- ifelse(conflict_raw$best!=0, 1,0)
+# generate dummy var to identify conflict 1 - Yes, 0 - No
+conflict_raw$armconf1 <- ifelse(conflict_raw$best!=0, 1,0)
 
+# retain distinct years, drop conflict_id
 conflict_cleaned <- distinct(conflict_raw,ISO,year,.keep_all = TRUE)
+conflict_cleaned <- subset(conflict_cleaned, select = -(conflict_id))
 
 write.csv(conflict_cleaned,here("data","conflict_clean.csv"))
